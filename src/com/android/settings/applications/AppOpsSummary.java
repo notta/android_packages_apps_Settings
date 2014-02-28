fe795f2523efd8ff1b5109d19286566e107c69ce
notta <mr.notta@gmail.com>
2014-02-28 11:29:19 -0500

    Settings: add white dependant and use it
    Change-Id: Ib834b4db6df973fd9b3d80739796296963b42d95
    
    Conflicts:
    	src/com/android/settings/applications/AppOpsSummary.java

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -98,6 +98,8 @@ public class AppOpsSummary extends Fragment {
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());
         mViewPager.setAdapter(adapter);
         mViewPager.setOnPageChangeListener(adapter);
+        PagerTabStrip tabs = (PagerTabStrip) rootView.findViewById(R.id.tabs);
+        tabs.setTabIndicatorColorResource(android.R.color.white);
 
         // We have to do this now because PreferenceFrameLayout looks at it
         // only when the view is added.
